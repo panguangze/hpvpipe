bff8e1f1a7ef77cb7af06f6c3785d77a0412cace
panguangze <guangzep@gmail.com>
2021-07-27 06:08:20 +0800

    tgs

--- tgs_scripts/pipe.sh
+++ tgs_scripts/pipe.sh
@@ -1,14 +1,13 @@
-src=/home/caronkey/Documents/cityu/hpv/simulate/scripts
 segs=$1
 ref=$2
 tgs_fa=$3
 outdir=$4
 junc_len=$5
 max_bias=$6
+src=$7
 
+cat $segs  |grep ^SEG|awk -F ':| ' '{print $3"\t"$4"\t"$5"\t"$6}' > $outdir/input.segs
 
-cat $segs  |grep ^SEG|awk -F ':| ' '{print $3"\t"$4"\t"$5"\t"$6}' >> $outdir/input.segs
- 
 cat $segs |grep ^JUNC|awk -F ':| ' '{print $3" "$4" "$6" "$7}' >$outdir/input.juncs
 
 perl $src/0.mk_junc_fa.pl $ref $outdir/input.segs $outdir/input.juncs $junc_len > $outdir/juncs.fa
@@ -21,5 +20,5 @@ sh $outdir/blast.sh
 perl $src/1.filter_m8.pl $outdir $junc_len > $outdir/filter_m8.0.8
 perl $src/2.mk_edge.pl $outdir/filter_m8.0.8 $junc_len  > $outdir/reads_juncs
 
-perl $src/check.pl $outdir/input.segs $outdir/reads_juncs $seg_seq $max_bias > $outdir/juncs.check
+perl $src/check.pl $outdir/input.segs $outdir/reads_juncs $max_bias > $outdir/juncs.check
 cat $outdir/juncs.check|grep -v BAD|sort|uniq > $outdir/tgs_juncs 
